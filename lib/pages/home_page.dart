14dc22586424b114a444db67cec1df160d3af803
Asmat Batool <110338738+AsmatBatool15@users.noreply.github.com>
2022-08-19 22:33:59 -0700

    update

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,10 +2,31 @@ import 'package:catalog_app/models/catalog.dart';
 import 'package:catalog_app/pages/widgets/drawer.dart';
 import 'package:catalog_app/pages/widgets/items_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   final int days = 30;
+
   final String = "ASMAT BATOOL";
+  
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+  loadData() async {
+     final catalogJason = await rootBundle.loadString("pages/files/catalog.jason");
+     final decodedData = jsonDecode(catalogJason);
+     var productsData = decodedData("products");
+     print(productsData);
+  }
+
   @override
   Widget build(BuildContext context) {
     final dummyList = List.generate(20, ((index) => CatalogModel.item[0]));
